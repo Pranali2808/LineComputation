ce0fc1e65c0f2789b174ea620c22ffbe8654d480
pranali2808 <ranepranali1997@gmail.com>
2022-04-01 16:14:22 +0530

    file added to UC2

--- Program.cs
+++ Program.cs
@@ -10,11 +10,31 @@ namespace LineComparisionComputation
     {
         static void Main(string[] args)
         {
-            LineComparision lineComparison = new LineComparision();
-            lineComparison.PointValues();
-            double lineLength = lineComparison.GetLength();
-            Console.WriteLine("Length of first Line according to points is= " + lineLength);
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            LineComparision firstLine = new LineComparision();
+            LineComparision secondLine = new LineComparision();
+            firstLine.PointValues();
+            double firstLineLength = firstLine.GetLength();
+            Console.WriteLine("Length of first Line: " + firstLineLength);
+            secondLine.PointValues();
+            double secondLineLength = secondLine.GetLength();
+            Console.WriteLine("Length of Second Line: " + secondLineLength);
+
+            bool status = LengthComparison(firstLineLength, secondLineLength);
+            if (status)
+            {
+                Console.WriteLine("Both the Line lengths are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Line Lengths are not Equal");
+            }
             Console.ReadLine();
+        }
+
+        public static Boolean LengthComparison(double firstLength, double secondLength)
+        {
+            return firstLength.Equals(secondLength);
         }
     }
 }
