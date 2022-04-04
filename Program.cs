535f72d78053412f6c7d2b7ced0028a02162d46c
pranali2808 <ranepranali1997@gmail.com>
2022-04-04 22:23:14 +0530

    compare length of 2 lines with compare method

--- Program.cs
+++ Program.cs
@@ -10,11 +10,6 @@ namespace LineComparisionComputation
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
-
-=======
->>>>>>> UC3_LenghthSmallGreate
             Console.WriteLine("Welcome to Line Comparison Computation program on Master Branch");
             LineComparision firstLine = new LineComparision();
             LineComparision secondLine = new LineComparision();
@@ -24,29 +19,21 @@ namespace LineComparisionComputation
             secondLine.PointValues();
             double secondLineLength = secondLine.GetLength();
             Console.WriteLine("Length of Second Line: " + secondLineLength);
-<<<<<<< HEAD
-
-            if (firstLineLength == secondLineLength)
-            {
-=======
-            if (firstLineLength > secondLineLength)
+            if (firstLineLength.CompareTo(secondLineLength) > 0)
             {
                 Console.WriteLine("Line1 is greater than Line2");
             }
-            else if (firstLineLength == secondLineLength)
+            else if (firstLineLength.CompareTo(secondLineLength) == 0)
+
             {
->>>>>>> UC3_LenghthSmallGreate
                 Console.WriteLine("Lines are equal");
             }
             else
             {
-<<<<<<< HEAD
-                Console.WriteLine("Lines are not equal");
-=======
                 Console.WriteLine("Line2 is greater than Line1");
->>>>>>> UC3_LenghthSmallGreate
             }
             Console.ReadLine();
+
         }
     }
 }
